268310d4977e6bba6d3264b2bffd3f114a488a1f
Pedro <pedrovs3@hotmail.com>
2023-01-27 21:59:09 -0300

    First commit, next step is refactoring and tests automation

---
+++ src/main/kotlin/com/pedrovieira/bankapi/AccountRepository.kt
@@ -0,0 +1,6 @@
+package com.pedrovieira.bankapi
+
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface AccountRepository : JpaRepository<Account, Long> {
+}
---
+++ src/test/kotlin/com/pedrovieira/bankapi/BankapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pedrovieira.bankapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BankapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
